0eb89ba8f29dff13a04fa71b74330f6c36866a9d
Mark Keenan <{ID}+{username}@users.noreply.github.com>
2020-06-26 00:00:23 +0100

    Updates

---
+++ precache-manifest.ee6bbcdf5c395478fc1cb301f3d89880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee9a556aa97cd14cad31868e95ca708",
+    "url": "/Tasklist-React/index.html"
+  },
+  {
+    "revision": "d396bc73ff6d6cdc8134",
+    "url": "/Tasklist-React/static/css/main.ad32dc2b.chunk.css"
+  },
+  {
+    "revision": "02e51cb3baaed51ac4c9",
+    "url": "/Tasklist-React/static/js/2.d38893ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tasklist-React/static/js/2.d38893ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d396bc73ff6d6cdc8134",
+    "url": "/Tasklist-React/static/js/main.474b8f95.chunk.js"
+  },
+  {
+    "revision": "bd92f3f6ca84697340f4",
+    "url": "/Tasklist-React/static/js/runtime-main.5e5520e5.js"
+  }
+]);
